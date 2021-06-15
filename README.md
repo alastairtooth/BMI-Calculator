b47eaeed24f442db1eb049cfcb55031440ea2eb5
Alastair Tooth <alastairtooth@gmail.com>
2021-06-15 11:29:14 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#  BMI Calculator
+
+ABody Mass Index calculator that - based on the user’s weight and height - will calculate their body mass and provide health advice. 
+
+# Required Skills
+
+* Creation of multi-screen apps with animated navigation.
+* Optional binding, optional chaining and the nil coalescing operator.
+* Creation and use of Classes
+* Pass by value vs. pass by reference. 
+* Formatting Strings. 
+* Color literals.
---
+++ ResultsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultsViewController.swift
+//  BMI Calculator
+//
+//  Created by Alastair Tooth on 7/8/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
